f51b107b1dd045c721d937a071371e3a1c491c3d
momoma-null <momoma.creation@gmail.com>
2026-06-10 14:54:25 +0900

    JsonNodeへのパースを共通化

--- Assets/UniGLTF/Runtime/UniGLTF/IO/Parser/GlbLowLevelParser.cs
+++ Assets/UniGLTF/Runtime/UniGLTF/IO/Parser/GlbLowLevelParser.cs
@@ -75,14 +75,15 @@ namespace UniGLTF
 
         public static GltfData ParseGltf(string path, string json, IReadOnlyList<GlbChunk> chunks, IStorage storage, MigrationFlags migrationFlags)
         {
-            var GLTF = GltfDeserializer.Deserialize(json.ParseAsJson());
+            var jsonNode = json.ParseAsJson();
+            var GLTF = GltfDeserializer.Deserialize(jsonNode);
             if (GLTF.asset.version != "2.0")
             {
                 throw new UniGLTFException("unknown gltf version {0}", GLTF.asset.version);
             }
 
             // Version Compatibility
-            RestoreOlderVersionValues(json, GLTF);
+            RestoreOlderVersionValues(jsonNode, GLTF);
 
             FixMeshNameUnique(GLTF);
             FixBlendShapeNameUnique(GLTF);
@@ -300,16 +301,15 @@ namespace UniGLTF
             }
         }
 
-        private static void RestoreOlderVersionValues(string Json, glTF GLTF)
+        private static void RestoreOlderVersionValues(JsonNode jsonNode, glTF GLTF)
         {
-            var parsed = UniJSON.JsonParser.Parse(Json);
             for (int i = 0; i < GLTF.images.Count; ++i)
             {
                 if (string.IsNullOrEmpty(GLTF.images[i].name))
                 {
                     try
                     {
-                        var extraName = parsed["images"][i]["extra"]["name"].Value.GetString();
+                        var extraName = jsonNode["images"][i]["extra"]["name"].Value.GetString();
                         if (!string.IsNullOrEmpty(extraName))
                         {
                             GLTF.images[i].name = extraName;
